222975c5eccdafcdb0b3762cb8033cf19fb7e5fb
Cas Eliens <cas.eliens@gmail.com>
2018-10-14 19:55:33 +0200

    update to version 1.0.1

--- manifest.sh
+++ manifest.sh
@@ -1,1 +1,1 @@
-export DOWNLOAD_FROM=https://github.com/reddit-diabetes/diabot-discord-v2/releases/download/1.0/diabot.jar
+export DOWNLOAD_FROM=https://github.com/reddit-diabetes/diabot-discord-v2/releases/download/1.0.1/diabot-1.0.1-RELEASE.jar
