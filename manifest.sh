cc6b4c05294e3216c37d3ea4330ea4c3fb78ba69
Cas Eliens <cas.eliens@gmail.com>
2018-10-14 20:01:22 +0200

    change download url for jar

--- manifest.sh
+++ manifest.sh
@@ -1,1 +1,1 @@
-export DOWNLOAD_FROM=https://github.com/reddit-diabetes/diabot-discord-v2/releases/download/1.0.1/diabot-1.0.1-RELEASE.jar
+export ARTIFACT_URL=https://github.com/reddit-diabetes/diabot-discord-v2/releases/download/1.0.1/diabot-1.0.1-RELEASE.jar
