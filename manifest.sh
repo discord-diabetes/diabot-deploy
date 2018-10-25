d9db23a683eb92dfe268e0ca69b3f89373f91163
Cas Eliens <cas.eliens@gmail.com>
2018-10-25 16:49:59 +0200

    update to 1.5.2

--- manifest.sh
+++ manifest.sh
@@ -1,1 +1,1 @@
-export ARTIFACT_URL=https://github.com/reddit-diabetes/diabot-discord/releases/download/1.5.1/diabot-1.5.1-RELEASE.jar
+export ARTIFACT_URL=https://github.com/reddit-diabetes/diabot-discord/releases/download/1.5.2/diabot-1.5.2-RELEASE.jar
