03ddbee52453d03832ab2d0cbfe17374ebf02a71
Cas Eliens <cas.eliens@gmail.com>
2018-10-28 17:24:04 +0100

    update to 1.7.0

--- manifest.sh
+++ manifest.sh
@@ -1,1 +1,1 @@
-export ARTIFACT_URL=https://github.com/reddit-diabetes/diabot-discord/releases/download/1.6.2/diabot-1.6.2-RELEASE.jar
+export ARTIFACT_URL=https://github.com/reddit-diabetes/diabot-discord/releases/download/1.7.0/diabot-1.7.0-RELEASE.jar
