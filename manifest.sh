5cd33ceab18a5587d3fa7c49d3979a8a715a1e86
Cas Eliëns <cas.eliens@isaac.nl>
2018-11-08 13:01:29 +0100

    update to 1.8.0

--- manifest.sh
+++ manifest.sh
@@ -1,1 +1,1 @@
-export ARTIFACT_URL=https://github.com/reddit-diabetes/diabot-discord/releases/download/1.7.3/diabot-1.7.3-RELEASE.jar
+export ARTIFACT_URL=https://github.com/reddit-diabetes/diabot-discord/releases/download/1.8.0/diabot-1.8.0.jar
