831115728cc7504b3e7d9e8687d1da8e1d2f4ae7
Cas Eliëns <cas.eliens@gmail.com>
2018-11-27 20:37:08 +0100

    update to 1.9.1

--- manifest.sh
+++ manifest.sh
@@ -1,1 +1,1 @@
-export ARTIFACT_URL=https://github.com/reddit-diabetes/diabot-discord/releases/download/1.9.0/diabot-1.9.0.jar
+export ARTIFACT_URL=https://github.com/reddit-diabetes/diabot-discord/releases/download/1.9.1/diabot-1.9.1.jar
