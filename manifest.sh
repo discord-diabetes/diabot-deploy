f1611bbab9c2d3082c45336d4a2b610aee02ee55
Cas Eliëns <cas.eliens@isaac.nl>
2018-12-06 08:36:26 +0100

    udpate to 1.9.3

--- manifest.sh
+++ manifest.sh
@@ -1,1 +1,1 @@
-export ARTIFACT_URL=https://github.com/reddit-diabetes/diabot-discord/releases/download/1.9.2/diabot-1.9.2.jar
+export ARTIFACT_URL=https://github.com/reddit-diabetes/diabot-discord/releases/download/1.9.3/diabot-1.9.3.jar
