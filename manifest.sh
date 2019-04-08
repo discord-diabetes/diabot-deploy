582dc03aa673623c002339eb42a6a658cb512fc6
Cas Eliëns <cas.eliens@gmail.com>
2019-04-08 19:01:59 +0200

    update to 1.9.5

--- manifest.sh
+++ manifest.sh
@@ -1,1 +1,1 @@
-export ARTIFACT_URL=https://github.com/reddit-diabetes/diabot-discord/releases/download/1.9.4/diabot-1.9.4.jar
+export ARTIFACT_URL=https://github.com/reddit-diabetes/diabot-discord/releases/download/1.9.5/diabot-1.9.5.jar
