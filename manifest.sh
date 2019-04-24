5cacbb3c129463637b43a75ad4188c78ee649951
Cas Eliëns <cas.eliens@isaac.nl>
2019-04-24 10:36:56 +0200

    Update to 1.11.1

--- manifest.sh
+++ manifest.sh
@@ -1,1 +1,1 @@
-export ARTIFACT_URL=https://github.com/reddit-diabetes/diabot-discord/releases/download/1.11.0/diabot-1.11.0.jar
+export ARTIFACT_URL=https://github.com/reddit-diabetes/diabot-discord/releases/download/1.11.1/diabot-1.11.1.jar
